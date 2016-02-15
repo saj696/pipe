f8fb08c7ae0d54a9be42cca637a3e276df559f8f
mazba <mazba.cse@gmail.com>
2016-02-15 14:03:51 +0600

    suppliers

--- config/common.php
+++ config/common.php
@@ -13,6 +13,7 @@ return [
     'workspace_type' => ['1'=>'Showroom', '2'=>'Delivery Center', '3'=>'Factory'],
     'pagination' => 5,
     'status' => ['1'=>'Active','0'=>'Inactive'],
-    'material_type' => ['1'=>'Normal', '2'=>'Color', '3'=>'Mixer']
+    'material_type' => ['1'=>'Normal', '2'=>'Color', '3'=>'Mixer'],
+    'supplier_types'=>[1=>'Local',2=>'International']
 
 ];
