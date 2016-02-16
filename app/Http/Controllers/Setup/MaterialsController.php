ca903a510e3fc4a6031dbc4f1faccaffafd6f88b
Kamrul Hasan <saj696@gmail.com>
2016-02-16 13:00:12 +0600

    RawStock.php

--- app/Http/Controllers/Setup/MaterialsController.php
+++ app/Http/Controllers/Setup/MaterialsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Setup;
 
 use App\Http\Requests;
 use App\Models\Material;
+use App\Models\RawStock;
 use Carbon\Carbon;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\MaterialRequest;
@@ -11,6 +12,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Request;
 use Session;
+use DB;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Pagination\Paginator;
 
@@ -43,16 +45,33 @@ class MaterialsController extends Controller
 
     public function store(MaterialRequest $request)
     {
-        $material = New Material;
-        $material->name = $request->input('name');
-        $material->type = $request->input('type');
-        $material->status = $request->input('status');
-        $material->created_by = Auth::user()->id;
-        $material->created_at = time();
+        DB::beginTransaction();
+        try
+        {
+            $material = New Material;
+            $material->name = $request->input('name');
+            $material->type = $request->input('type');
+            $material->status = $request->input('status');
+            $material->created_by = Auth::user()->id;
+            $material->created_at = time();
+            $material->save();
+            $insertedId = $material->id;
 
-        $material->save();
+            $rawStock = New RawStock;
+            $rawStock->material_id = $insertedId;
+            $rawStock->created_by = Auth::user()->id;
+            $rawStock->created_at = time();
+            $rawStock->save();
 
-        Session()->flash('flash_message', 'Material has been created!');
+            DB::commit();
+            Session()->flash('flash_message', 'Material has been created!');
+        }
+        catch (\Exception $e)
+        {
+            DB::rollback();
+            Session()->flash('flash_message', 'Material not created!');
+        }
+
         return redirect('materials');
     }
 
---
+++ app/Models/RawStock.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+
+class RawStock extends Model
+{
+    public $timestamps=false;
+
+    protected $fillable = [
+        'material_id',
+        'quantity'
+    ];
+
+    public function material()
+    {
+        return $this->belongsTo('App\Models\Material');
+    }
+}
